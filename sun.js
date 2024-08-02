c48ea2c0aa8edcb46c147cd12e964fba64cad448
Amuthan Mannar <amuthanmannar4@gmail.com>
2024-08-02 22:50:47 +0530

    Create sun.js

---
+++ sun.js
@@ -0,0 +1,69 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: purple; icon-glyph: magic;
+
+
+
+const openweather_api_key = ""
+
+
+
+function convertUnixTimeToLocalTime(unixTime, timezoneOffset) {
+    const date = new Date((unixTime)*1000);
+    console.log(date);
+    let df_one = new DateFormatter();
+    df_one.dateFormat = 'h:m a';
+    return df_one.string(date);
+}
+
+
+
+let latLong = {}
+try {
+  latLong = await Location.current()
+} catch {}
+
+
+
+let req = new Request('https://api.openweathermap.org/data/2.5/weather?units=metric&lat='+latLong.latitude+'&'+'lon='+latLong.longitude+'&appid='+openweather_api_key);
+req.method = "get";
+let weather = await req.loadJSON();
+
+let sun_rise = weather['sys']["sunrise"]
+let sun_set = weather['sys']["sunset"]
+let timezone = weather['timezone']
+
+console.log(sun_rise);
+console.log(sun_set);
+console.log(timezone);
+
+/////
+
+let states = [
+  "சூரியன்",
+  convertUnixTimeToLocalTime(sun_rise, timezone),
+  "முதல்",
+  convertUnixTimeToLocalTime(sun_set, timezone),
+  "வரை"
+];
+
+let font = new Font("GillSans-Light", 11);
+
+
+
+let widget = new ListWidget();
+
+for (const x of states){ 
+  var one = widget.addText(x);
+  // one.font = Font.thinMonospacedSystemFont(11);
+  one.font = font;
+  one.centerAlignText();
+  
+}
+
+
+widget.presentSmall(); 
+
+
+Script.setWidget(widget);
+Script.complete();
